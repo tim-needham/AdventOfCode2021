d0ed39f53fdadb244b95a9750309e6274059a9fd
tneedham <tneedham@travelrepublic.co.uk>
2021-12-06 11:57:52 +0000

    Day 6 - Move memoization to module

--- AdventOfCode2021/Day6.fs
+++ AdventOfCode2021/Day6.fs
@@ -1,5 +1,6 @@
 module Day6
 
+open Memo;
 open System;
 open System.Diagnostics;
 open System.IO;
@@ -9,29 +10,12 @@ let parse (input : string) : int list =
     |> Seq.toList
     |> List.map Int32.Parse;
 
-// memoize a given function
-let memoize (f : 'TArg -> 'TResult) : ('TArg -> 'TResult) =
-    let cache = new System.Collections.Generic.Dictionary<_, _>();
-
-    (fun x ->
-        match cache.TryGetValue x with
-        | true, v -> v;
-        | false, _ ->   let v = f x;
-                        cache.Add(x, v);
-                        v);
-
-// memoize only works on functions with one parameter so use this overload for 2 parameters
-let memoize2 (f': 'TArg1 -> 'TArg2 -> 'TResult) : ('TArg1 -> 'TArg2 -> 'TResult) =
-    let f = memoize (fun (a, b) -> f' a b);
-    (fun a b -> f(a, b));
-
 // generate fish using memoization
 let rec generate (clock : int) (fish : int) : int64 =
     match clock, fish with
     | 0, _ -> 1L;
     | c, 0 -> (generateMemo (c - 1) 8) + (generateMemo (c - 1) 6);
     | c, n -> generateMemo (c - 1) (n - 1);
-
 and generateMemo = memoize2 generate;
 
 let run (file : string, testMode : bool) =
---
+++ AdventOfCode2021/Utils/Memo.fs
@@ -0,0 +1,22 @@
+﻿module Memo
+
+// memoize a given function
+let memoize (f : 'TArg -> 'TResult) : ('TArg -> 'TResult) =
+    let cache = new System.Collections.Generic.Dictionary<_, _>();
+
+    (fun x ->
+        match cache.TryGetValue x with
+        | true, v -> v;
+        | false, _ ->   let v = f x;
+                        cache.Add(x, v);
+                        v);
+
+// memoize only works on functions with one parameter so use this overload for 2 parameters
+let memoize2 (f': 'TArg1 -> 'TArg2 -> 'TResult) : ('TArg1 -> 'TArg2 -> 'TResult) =
+    let f = memoize (fun (a, b) -> f' a b);
+    (fun a b -> f(a, b));
+
+// memoize only works on functions with one parameter so use this overload for 3 parameters
+let memoize3 (f': 'TArg1 -> 'TArg2 -> 'TArg3 -> 'TResult) : ('TArg1 -> 'TArg2 -> 'TArg3 -> 'TResult) =
+    let f = memoize (fun (a, b, c) -> f' a b c);
+    (fun a b c -> f(a, b, c));
