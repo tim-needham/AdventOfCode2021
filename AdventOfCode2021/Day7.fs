66729d1f2e05c260be99981a944a6687514d19e6
tneedham <tneedham@travelrepublic.co.uk>
2021-12-07 10:48:48 +0000

    Day 7

--- AdventOfCode2021/Day7.fs
+++ AdventOfCode2021/Day7.fs
@@ -1,16 +1,63 @@
 module Day7
 
+open Memo;
+open System;
 open System.Diagnostics;
+open System.IO;
 
+let parse (input : string) : int list =
+    input.Split([| "," |], StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.toList
+    |> List.map Int32.Parse;
+
+// compute triangular numbers, I knew I'd need memoization again...
+let rec triangular (n : int) : int =
+    if n < 1 then
+        0;
+    else
+        n + triangularMemo (n - 1);
+and triangularMemo = memoize triangular;
+
+let fuelCostConstant (position : int) (crabs : int list) : int =
+    crabs
+    |> List.sumBy (fun x -> Math.Abs(position - x));
+
+let fuelCostTriangular (position : int) (crabs : int list) : int =
+    crabs
+    |> List.sumBy (fun x -> Math.Abs(position - x) |> triangularMemo);
+
+// moving across the position space in order, results will decrease then increase so stop when we start increasing
+let rec bestFuel (fuelCost : int option) (crabs : int list) (cost : int -> int list -> int) (positions : int list) : int =
+    match positions, fuelCost with
+    | [], None -> failwith "Unable to calculate best fuel!";
+    | [], Some f -> f;
+    | x::xs, None -> bestFuel (Some (cost x crabs)) crabs cost xs;
+    | x::xs, Some f ->  let f' = cost x crabs;
+                        if f' > f then
+                            f;
+                        else
+                            bestFuel (Some f') crabs cost xs;
+
+let solve (cost : int -> int list -> int) (crabs : int list) : int =
+    [List.min crabs..List.max crabs]
+    |> bestFuel None crabs cost;
+    
 let run (file : string, testMode : bool) =
 
     let w = new Stopwatch();
     w.Start();
 
-    0
+    let input = File.ReadAllLines(file)
+                |> Seq.toList
+                |> List.collect parse;
+
+    // seems like you can solve part 1 using the median value of the list of crabs... obviously doesn't work for part 2 though
+    input
+    |> solve fuelCostConstant
     |> printfn "Day 7, part 1: %d";
 
-    0
+    input
+    |> solve fuelCostTriangular
     |> printfn "Day 7, part 2: %d";
 
     w.Stop();
