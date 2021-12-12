427aa23677a55a965ecac7dbe86ff7c0eb8bd962
tneedham <tneedham@travelrepublic.co.uk>
2021-12-12 10:12:16 +0000

    Day 12

--- AdventOfCode2021/Day12.fs
+++ AdventOfCode2021/Day12.fs
@@ -1,16 +1,51 @@
 module Day12
 
+open System;
 open System.Diagnostics;
+open System.IO;
+
+let parse (input : string) : (string * string) list =
+    let p = input.Split([| "-" |], StringSplitOptions.RemoveEmptyEntries);
+    // connections work both ways
+    [ (p.[0], p.[1]); (p.[1], p.[0]) ];
+
+let bigCave (input : string) : bool =
+    List.contains input.[0] ['A'..'Z'];
+
+let rec traverse (target : string) (path : string list) (twice : bool) (connections : (string * string) list) : seq<string list> =
+    seq {
+        match List.head path with
+        | x when x = target -> yield path;
+        | x ->  let caves = connections
+                            |> List.filter (fun (a, b) -> b = x && (not (List.contains a path) || not twice || bigCave a));
+                
+                if List.length caves > 0 then
+                    yield!  caves
+                            |> Seq.collect (fun (a, _) ->   match bigCave a with
+                                                            | true -> traverse target (a::path) twice connections;
+                                                            | false -> traverse target (a::path) (twice || List.contains a path) connections;
+                                            );
+    };
 
 let run (file : string, testMode : bool) =
 
     let w = new Stopwatch();
     w.Start();
 
-    0
+    let input = File.ReadAllLines(file)
+                |> Seq.toList
+                |> List.collect parse
+                // we can make part 2 easier by ensuring the start and end connections are 1 way only...
+                |> List.filter (fun (a, b) -> b <> "start" && a <> "end");
+
+    input
+    |> traverse "start" ["end"] true
+    |> Seq.length
     |> printfn "Day 12, part 1: %d";
 
-    0
+    input
+    |> traverse "start" ["end"] false
+    |> Seq.length
     |> printfn "Day 12, part 2: %d";
 
     w.Stop();
